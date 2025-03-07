fa583e1665ad6cb694b6f00e7d3e4b147ba097c1
Victor-Egas <victor.egas.fieet@gmail.com>
2025-03-07 00:29:00 -0500

    init commit

---
+++ src/main/java/com/provider/exchange/provider_exchange/ProviderExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.provider.exchange.provider_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/provider/exchange/provider_exchange/ProviderExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provider.exchange.provider_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
